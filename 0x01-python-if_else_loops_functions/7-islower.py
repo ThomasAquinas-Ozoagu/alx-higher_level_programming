6e336b314307f243b0425e3ffa63e7d465ef9e3d
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-13 14:28:35 +0000

    sepatate lower case from others

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
 def islower(c):
-    if ord(c) > 96:
-        if ord(c) < 123:
-            return True
+    if ord(c) > 96 and ord(c) < 123:
+        return True
 
     else:
         return False
