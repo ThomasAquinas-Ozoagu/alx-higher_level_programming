707b62729e126bc91800e11f026945752a02fc18
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-21 20:51:40 +0000

    Use C style to format header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,2 +1,15 @@
-def print_list_integer(my_list=[])
-def element_at(my_list, idx)
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#ifndef _PRINT_LIST_INTEGER_
+#define _PRINT_LIST_INTEGER_
+def print_list_integer(my_list=[]);
+#endif /*  _PRINT_LIST_INTEGER_ */
+
+#ifndef _ELEMENT_AT_
+#define _ELEMENT_AT_
+def element_at(my_list, idx);
+#endif /*  _ELEMENT_AT_ */
+
+
+#endif /* _LISTS_H_ */
