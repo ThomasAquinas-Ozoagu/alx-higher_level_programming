d9bb267ed195bb855c238310a76b13d2b0a89768
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-23 23:28:25 +0000

    format as integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     size = len(matrix[0])
     for i in range(len(matrix)):
         for j in range(size):
-            print("{}".format(matrix[i][j]),
+            print("{:d}".format(matrix[i][j]),
                   end=" " if j < (size - 1) else "")
         print()
