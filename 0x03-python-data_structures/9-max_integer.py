a80f17987c3d02d4d94d329dff2d851bb5750884
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-24 00:28:42 +0000

    consider negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,9 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
 
+    if my_list[0] < 0:
+        maxi = my_list[0]
+
     for i in range(len(my_list)):
         if my_list[i] > maxi:
             maxi = my_list[i]
