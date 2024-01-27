0bd6b300b72f683398989d6be81fb933a5daf69a
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-27 23:17:28 +0000

    sort that dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_list = list(a_dictionary)
+    sorted_list.sort()
+    for x in sorted_list:
+        print ("{}: {}".format(x, a_dictionary[x]))
