a6b544d22a442706eb2a4271895014f682eea108
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 18:30:34 +0000

    space after a while statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     e = 0
-    while(e < x):
+    while (e < x):
         try:
             print("{}".format(my_list[e]), end="")
         except IndexError:
             break
-        e+=1
+        e += 1
     print()
     return (e)
