cd7a301246a103aa6d573a23d591c015ca27f966
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 18:26:45 +0000

    remove space around arithmetic operator

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[e]), end="")
         except IndexError:
             break
-        e += 1
+        e+=1
     print()
     return (e)
