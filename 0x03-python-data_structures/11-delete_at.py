8eb95b684165ceca2a3bfac7ac1595a4c226b288
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 11:27:48 +0000

    put returned value inside ()

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,9 +4,9 @@
 def delete_at(my_list=[], idx=0):
 
     if idx < 0:
-        return my_list
+        return (my_list)
     elif idx > (len(my_list) - 1):
-        return my_list
+        return (my_list)
     else:
         del(my_list[idx])
-        return my_list
+        return (my_list)
