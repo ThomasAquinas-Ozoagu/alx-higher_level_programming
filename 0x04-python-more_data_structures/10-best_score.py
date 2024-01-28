e01d943bf9b607bbfca46d52e9747a466d6cbf76
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 12:21:41 +0000

    use is instead of == when comparing to objects like None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary == {} or a_dictionary == None:
+    if a_dictionary == {} or a_dictionary is None:
         return None
     keys = list(a_dictionary)
     bigg = a_dictionary[keys[0]]
