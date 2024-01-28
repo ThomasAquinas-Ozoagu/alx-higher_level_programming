1571a5582cefc3199eaf372e35bb89ac7a745ee8
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 14:56:10 +0000

    troubleshoot everyline when your code is not working

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,11 +5,13 @@ def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string == "":
         return (0)
 
-    sum = 0
-    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for a in roman_string:
-        if conv[a] > sum:
-            sum = conv[a] - sum
+    suma = 0
+    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+            'M': 1000}
+    for a in range(len(roman_string)):
+        if a > 0 and conv[roman_string[a]] > conv[roman_string[a-1]]:
+            suma += conv[roman_string[a]]
+            suma -= 2*conv[roman_string[a - 1]]
         else:
-            sum += conv[a]
-    return (sum)
+            suma += conv[roman_string[a]]
+    return (suma)
