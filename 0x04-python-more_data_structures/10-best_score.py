7bfff5dc3b6c6e230d7fd3bba4f3b5370d05c9c4
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 12:19:20 +0000

    compare dictionary values

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if a_dictionary == {} or a_dictionary == None:
+        return None
+    keys = list(a_dictionary)
+    bigg = a_dictionary[keys[0]]
+    bigKey = keys[0]
+
+    for x in keys:
+        if a_dictionary[x] > bigg:
+            bigg = a_dictionary[x]
+            bigKey = x
+
+    return (bigKey)
