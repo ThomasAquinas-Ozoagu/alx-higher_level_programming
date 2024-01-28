4d140014bf6f6e0f87284f5666f6d289d902d688
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 11:16:27 +0000

    reduce two lines to 1 whenever possible

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,15 +2,14 @@
 
 
 def max_integer(my_list=[]):
-    maxi = 0
+
     if my_list == []:
         return None
 
-    if my_list[0] < 0:
-        maxi = my_list[0]
+    maxi = my_list[0]
 
     for i in range(len(my_list)):
         if my_list[i] > maxi:
             maxi = my_list[i]
 
-    return maxi
+    return (maxi)
