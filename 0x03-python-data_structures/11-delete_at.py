90044bdd485d833a954f681559731cafdd3cac2e
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 11:33:36 +0000

    consider empty lists

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,9 +3,11 @@
 
 def delete_at(my_list=[], idx=0):
 
-    if idx < 0:
+    if my_list == []:
         return (my_list)
-    elif idx > (len(my_list) - 1):
+    elif idx < 0:
+        return (my_list)
+    elif idx >= len(my_list):
         return (my_list)
     else:
         del(my_list[idx])
