d485200b37f4e97fbb6c630a749a3471444cc2b6
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 11:12:14 +0000

    no space after return

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,4 +7,4 @@ def no_c(my_string):
         if my_string[i] != 'c':
             if my_string[i] != 'C':
                 new_string += my_string[i]
-    return(new_string)
+    return (new_string)
