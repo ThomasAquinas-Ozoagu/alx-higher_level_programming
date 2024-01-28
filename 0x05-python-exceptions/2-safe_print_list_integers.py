60dbb2e00ef6e16d9425b71e5415068a90a38434
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 18:43:36 +0000

    Remove whitespace after while

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     e = 0
     f = 0
-    while(e < x):
+    while (e < x):
         try:
             print("{:d}".format(my_list[e]), end="")
             f += 1
