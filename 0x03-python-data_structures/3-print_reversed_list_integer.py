dedf02faf8d77a2a27442100ab1e2b5c118f121f
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-01-28 11:05:21 +0000

    use while instead of range

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    size = len(my_list) - 1
+    while size >= 0:
+        print("{:d}".format(my_list[size]))
+        size -= 1
