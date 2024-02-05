d9bab9fc1152df4480da877ad1f75c45045ffe24
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-05 23:52:03 +0000

    my first private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" My first step to understanding OOP, I'm going to have so much fun"""
+
+
+class Square:
+    """ This is an empty class, the methods/attributes will come later"""
+    def __init__(self, size):
+        """ This initializes the attributes of the class"""
+        self.__size = size
