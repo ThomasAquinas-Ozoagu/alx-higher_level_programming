af640b4ac750651a369a1cf189709580e626f36b
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-05 23:11:09 +0000

    Finally figured out difference between class and module documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+""" My first step to understanding OOP, I'm going to have so much fun"""
 
 
 class Square:
-    """ This is an empty class, the methods and attributes will come later"""
+    """ This is an empty class, the methods/attributes will come later"""
