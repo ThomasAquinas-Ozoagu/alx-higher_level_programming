ca74d7b948e9454ddf0d5f69b7ea0fed9288e480
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-11 19:32:29 +0000

    use @property for getters and @var.setter to set the private var

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,28 +6,22 @@ class Square:
     """ This is an empty class, the methods/attributes will come later"""
     def __init__(self, size=0):
         """ This initializes the attributes of the class"""
+        self.size = size
+
+    @property
+    def size(self):
+        """ This method returns the value of the private attribute"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, size):
+        """ This method modifies the private attribute"""
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-
-    def size(self, value=None):
-        print("the value is {}".format("value"))
-        """ This method returns the private attribute"""
-        if not value:
-            value = self.__size
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def size(self):
-        """ This method returns the private attribute"""
-        return (self.__size)
 
     def area(self):
         return (self.__size * self.__size)
