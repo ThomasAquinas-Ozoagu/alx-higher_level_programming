8468455a49a8915ee3e0a08abe16614dd2a2e5c8
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-11 20:36:03 +0000

    commit anyway

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,8 +32,7 @@ class Square:
     @position.setter
     def position(self, position):
         """ This method returns the value of the private attribute"""
-        if type(position) != tuple or position[0] < 0
-        or position[1] < 0:
+        if type(position) != tuple or position[0] < 0 or position[1] < 0:
             raise TypeError("psition must be a tuple of 2 positive integers")
         else:
             self.__position = position
