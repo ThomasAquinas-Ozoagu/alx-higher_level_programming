b82f4365f04b3106f58e86a971ea75637b20ecbc
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-15 18:19:30 +0000

    check number of test lines

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
 Prototype: def add_integer(a, b=98):
-a and b must be integers or floats, otherwise raise a TypeError exception 
+a and b must be integers or floats, otherwise raise a TypeError exception
 with the message a must be an integer or b must be an integer
 """
+
+
 def add_integer(a, b=98):
-    """ a function that adds 2 integers 
-    a and b must be first casted to integers if they are float
+    """
+    a function that adds 2 integers a and b
     """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
