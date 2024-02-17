0b1c64164ca6e375dafd1e07ae50a9240696d012
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-17 01:16:09 +0000

    create a simple class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" My first step to understanding OOP, I'm going to have so much fun"""
+
+
+class Rectangle:
+        """ This is an empty class, the methods/attributes will come later"""
+        pass
