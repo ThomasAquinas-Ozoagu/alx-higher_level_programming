9c4d93723941351de30d61bf240fb029f20913a4
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-18 00:17:11 +0000

    asign values

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,6 +21,8 @@ class Rectangle:
                 raise TypeError("width must be an integer")
             if width < 0:
                 raise ValueError("width must be >= 0")
+            else:
+                self.__width = width
 
         @property
         def height(self):
@@ -34,3 +36,5 @@ class Rectangle:
                 raise TypeError("height must be an integer")
             if height < 0:
                 raise ValueError("height must be >= 0")
+            else:
+                self.__height = height
