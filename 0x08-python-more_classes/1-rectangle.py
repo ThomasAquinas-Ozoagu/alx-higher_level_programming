ac4f8d36951757e78ec205e5d582b5b1b6a4e778
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-18 00:11:17 +0000

    document the code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,10 +11,12 @@ class Rectangle:
 
         @property
         def width(self):
+            """ This method returns the private attribute"""
             return self.__width
 
         @width.setter
         def width(self, width):
+            """ This method modifies the private attribute"""
             if type(width) != int:
                 raise TypeError("width must be an integer")
             if width < 0:
@@ -22,10 +24,12 @@ class Rectangle:
 
         @property
         def height(self):
+            """ This method returns the private attribute"""
             return self.__height
 
         @height.setter
         def height(self, height):
+            """ This method modifies the private attribute"""
             if type(height) != int:
                 raise TypeError("height must be an integer")
             if height < 0:
