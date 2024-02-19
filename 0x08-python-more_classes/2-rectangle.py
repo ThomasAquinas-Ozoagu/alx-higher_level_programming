f6c65ee97f91120476c6fa5bb047f03e97713e72
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-19 23:18:20 +0000

    add area and perimeter methods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -20,21 +20,29 @@ class Rectangle:
         return self.__height
 
     @width.setter
-    def width(self, widt):
+    def width(self, value):
         """ This method modifies the private attribute"""
-        if type(widt) != int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
-        if widt < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = widt
+            self.__width = value
 
     @height.setter
-    def height(self, heigh):
+    def height(self, value):
         """ This method modifies the private attribute"""
-        if type(heigh) != int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
-        if heigh < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = heigh
+            self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * (self.__height + self.__width)
