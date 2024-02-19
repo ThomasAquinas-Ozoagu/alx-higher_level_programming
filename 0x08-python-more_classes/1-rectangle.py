121beba92621f8e1bed22d52b8d28cb0d5ba2721
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-02-19 22:06:30 +0000

    correct the indentation error and privatize the first appearance

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,35 +6,35 @@ class Rectangle:
     """ This is an empty class, the methods/attributes will come later"""
     def __init__(self, width=0, height=0):
         """ This is an instantiation with optional values """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
-        @property
-        def width(self):
-            """ This method returns the private attribute"""
-            return self.__width
+    @property
+    def width(self):
+        """ This method returns the private attribute"""
+        return self.__width
 
-        @width.setter
-        def width(self, width):
-            """ This method modifies the private attribute"""
-            if type(width) != int:
-                raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = width
+    @width.setter
+    def width(self, width):
+        """ This method modifies the private attribute"""
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
 
-        @property
-        def height(self):
-            """ This method returns the private attribute"""
-            return self.__height
+    @property
+    def height(self):
+        """ This method returns the private attribute"""
+        return self.__height
 
-        @height.setter
-        def height(self, height):
-            """ This method modifies the private attribute"""
-            if type(height) != int:
-                raise TypeError("height must be an integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            else:
-                self.__height = height
+    @height.setter
+    def height(self, height):
+        """ This method modifies the private attribute"""
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
