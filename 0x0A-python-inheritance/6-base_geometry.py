0fca55d00fc219e9ee2276f5954f61387b71cd57
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-03-11 23:45:30 +0000

    raise and Exception for methods not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" An empty class """
+
+
+class BaseGeometry:
+    """ This class will be used to practice how to display default
+    class parameters'
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
