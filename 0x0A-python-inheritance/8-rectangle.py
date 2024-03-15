9b2dd5cb03343e03f0e5957f9d65626b8b681563
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-03-15 04:51:37 +0000

    do not initialize the variables

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,7 +24,13 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ Instantiation of Rectangle class """
         BaseGeometry.__init__(self)
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        """self.__width = width
+        self.__height = height"""
+
+        def __width(self):
+            self.integer_validator("width", width)
+            self.__width = width
+
+        def __height(self):
+            self.integer_validator("height", height)
+            self.__height = height
