44725c7288bc7fce9346485e526a1b4aa8ec6ec8
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-03-20 17:51:03 +0000

    use import again

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 """ Inheritance demonstration """
 
-class BaseGeometry:
-    """ This class will be used to practice how to display default
-    class parameters'
-    """
-    def area(self):
-        """ raises an Exception """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ validates the variable VALUE, whose name is a STRING """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
