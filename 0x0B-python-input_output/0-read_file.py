550507fc4722ca929bea86c74dd1a39b384c50b2
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-03-27 23:16:46 +0000

    Two blank lines

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
 """ Read file and print the content to standard output """
+
+
 def read_file(filename=""):
     """ a function that reads a text file (UTF8) and prints it to stdout """
     with open(filename, encoding="utf-8") as f:
