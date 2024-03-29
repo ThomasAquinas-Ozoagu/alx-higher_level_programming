ebf434947eb81b9ee48736f527fd9711101c031b
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-03-29 08:08:30 +0000

    pycodestyle

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,5 +4,5 @@ import json
 
 
 def to_json_string(my_obj):
-    """ a function that returns the JSON representation of an object (string)"""
+    """a function that returns the JSON representation of an object (string)"""
     return json.dumps(my_obj, sort_keys=True)
