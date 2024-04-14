b25795f5852d83a6f3de16f9cd0130b04f80c5f3
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-04-14 20:40:10 +0000

    it pays to understand

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,11 +8,13 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Class  constructor """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.set_width(width)
+        self.set_height(height)
+        self.set_x(x)
+        self.set_y(y)
+
         super().__init__()
+
         if id is not None:
             self.id = id
         else:
@@ -22,30 +24,80 @@ class Rectangle(Base):
         """ Returns the width """
         return self.__width
 
-    def set_width(self, value):
+    def set_width(self, width):
         """ Sets the width """
-        self.__width = value
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
 
     def get_height(self):
         """ Returns the height """
         return self.__height
 
-    def set_height(self, value):
+    def set_height(self, height):
         """ Sets the height """
-        self.__height = value
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
 
     def get_x(self):
         """ Returns the x """
         return self.__x
 
-    def set_x(self, value):
+    def set_x(self, x):
         """ Sets the x """
-        self.__x = value
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
 
     def get_y(self):
         """ Returns the y """
         return self.__y
 
-    def set_y(self, value):
+    def set_y(self, y):
         """ Sets the y """
-        self.__y = value
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    @property
+    def width(self):
+        """ getter for width """
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """ getter for width """
+        self.set_width(value)
+
+    @property
+    def height(self):
+        """ getter for height """
+        return self.__height
+    @height.setter
+    def height(self, value):
+        """ getter for heigt """
+        self.set_height(value)
+
+    @property
+    def x(self):
+        """ getter for width """
+        return self.__x
+    @x.setter
+    def x(self, value):
+        self.set_x(value)
+
+    @property
+    def y(self):
+        """ getter for width """
+        return self.__y
+    @y.setter
+    def y(self, value):
+        self.set_y(value)
