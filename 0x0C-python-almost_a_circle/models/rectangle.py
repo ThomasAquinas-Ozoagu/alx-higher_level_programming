a4c01df9f7f870125b35de311f8d06a8a96545d8
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-04-14 16:06:14 +0000

    Check again what the problem

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """  inherits from Base """
-#    self.__nb_objects = super.__nb_objects
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Class  constructor """
         self.__width = width
