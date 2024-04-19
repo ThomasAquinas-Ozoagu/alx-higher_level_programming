d089b294d8135c6dcee77247cc1b4ccf88850dc8
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-04-19 22:15:39 +0000

    check the difference between the args of function call and that of the main call

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -130,3 +130,15 @@ class Rectangle(Base):
         return ("[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
                                                  self.id, self.__x, self.__y,
                                                  self.__width, self.__height))
+
+    def update(self, *args):
+        if len(args) >0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
