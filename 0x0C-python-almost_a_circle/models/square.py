a347dbaffb0713d2457d395f86a37773698b979b
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-04-21 11:11:54 +0000

    private classes can only be called from inside the class itself, not even from a child class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" And now, the Square! """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ inherits from Rectangle """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Class  constructor """
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        """ The string representation for the class """
+        return "{} ({}) {}/{} - {}".format(self.__class__.__name__, self.id, self.x, self.y, self.width)
