fc499276ca4c14b0632d45c596169dae78167149
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-05-01 22:17:16 +0000

    semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,13 @@
 
 /* a script that prints a message depending of the number of arguments passed */
 
-let answer = 'No argument'
+let answer = 'No argument';
 
 if (process.argv[2]) {
   answer = 'Argument found';
 }
 
 if (process.argv[3]) {
-    answer = 'Arguments found';
+  answer = 'Arguments found';
 }
 console.log(answer);
