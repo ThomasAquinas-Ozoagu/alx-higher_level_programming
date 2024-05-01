d9b732ce511091236ac5535ef1ccc5cfcb3a6184
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-05-01 22:15:36 +0000

    go straight to the point

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* a script that prints a message depending of the number of arguments passed */
+
+let answer = 'No argument'
+
+if (process.argv[2]) {
+  answer = 'Argument found';
+}
+
+if (process.argv[3]) {
+    answer = 'Arguments found';
+}
+console.log(answer);
