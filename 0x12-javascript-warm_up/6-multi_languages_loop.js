8dd75f472ce563c34c0cbeb89026dbae55a90c44
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-05-07 23:36:48 +0000

    find the material that goes straight to the point

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* a script that prints 3 lines by using an array of string and a loop */
+const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < phrases.length; i++) {
+  console.log(phrases[i]);
+}
