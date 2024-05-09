f9616cc1dd09947df62204f1de2c8e2b1a239368
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-05-09 10:27:36 +0000

    use the correct search keywords

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/* a script that prints x times “C is fun” */
+
+const arg1 = Number(process.argv[2]);
+const roww = 'x';
+
+
+if (isNaN(arg1)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg1; i++) {
+    console.log(`${roww.repeat(arg1)}`);
+  }
+}
