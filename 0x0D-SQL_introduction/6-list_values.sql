abfc13874af82393564997170e710bf51ea97923
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-06-12 22:59:29 +0000

    Show table full description

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
 -- a script that prints the full description of the table first_table
 -- The database name will be passed as argument of mysql command
-SHOW CREATE TABLE first_table;
+SELECT * FROM first_table;
