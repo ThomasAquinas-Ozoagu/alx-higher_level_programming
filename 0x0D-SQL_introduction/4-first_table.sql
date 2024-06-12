4aca1371b912d8430a98a76a6a9c6da205715d46
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-06-12 12:48:34 +0000

    create my first table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- a script that creates a table called first_table in the current database
+-- The database name will be passed as argument of mysql command
+CREATE TABLE IF NOT EXISTS first_table (id INt, name VARCHAR(256));
