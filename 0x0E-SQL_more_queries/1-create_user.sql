0c31a67105aed9019f051c9c34d6745f255271f4
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-07 22:59:53 +0000

    create new db and new user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- a script that creates the MySQL server user user_0d_1.
 -- user_0d_1 should have all privileges on your MySQL server
 CREATE USER if not exists 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost'= 'user_0d_1_pwd';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
