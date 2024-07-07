5563c52a3670d0f0f11a761539c10b6e407b9765
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-07 22:03:08 +0000

    create new user with root access

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the MySQL server user user_0d_1.
 -- user_0d_1 should have all privileges on your MySQL server
 CREATE USER if not exists 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
