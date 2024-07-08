60327cafb1ae247bad723f1e5183d506ab0d7dca
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-08 23:03:10 +0000

    first table in hbtn_0d_2

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- a script that creates the table force_name on your MySQL server.
+-- The database name will be passed as an argument
+CREATE TABLE IF NOT EXISTS force_name (
+id INT,
+name VARCHAR(256) NOT NULL);
