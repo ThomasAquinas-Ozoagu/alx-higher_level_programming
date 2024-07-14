78ec73cdfa698bb7f1d728af8491b1148e2071a8
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-14 17:39:13 +0000

    order properly

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- a script that uses the hbtn_0d_tvshows database to lists
 -- all genres of the show Dexter
 
- SELECT DISTINCT title, CASE WHEN genre_id IS NULL THEN NULL ELSE name END
+ SELECT DISTINCT title,
+ CASE WHEN genre_id IS NULL THEN NULL ELSE name END as name
  FROM tv_genres, tv_shows LEFT JOIN tv_show_genres ON id = show_id
- WHERE tv_genres.id = genre_id OR genre_id IS NULL ORDER BY title;
+ WHERE tv_genres.id = genre_id OR genre_id IS NULL ORDER BY title, name;
