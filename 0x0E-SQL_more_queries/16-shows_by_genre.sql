e1a974b60ad43d29846408fdbe97a79fc1d50338
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-14 18:31:11 +0000

    you can do two joins in one SELECT statement

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- a script that uses the hbtn_0d_tvshows database to lists
 -- all genres of the show Dexter
 
- SELECT DISTINCT title,
- CASE WHEN genre_id IS NULL THEN NULL ELSE name END AS name
- FROM tv_genres, tv_shows LEFT JOIN tv_show_genres ON id = show_id
- WHERE tv_genres.id = genre_id OR genre_id IS NULL ORDER BY title, name;
+ SELECT title, name
+ FROM tv_shows
+ LEFT JOIN tv_show_genres ON tv_shows.id = show_id
+ LEFT JOIN tv_genres ON tv_genres.id = genre_id
+ ORDER BY title, name;
