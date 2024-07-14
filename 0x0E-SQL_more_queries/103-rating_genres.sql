946631d93c05cd7b42d7b37f6c1043f743f9fb74
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-14 17:32:05 +0000

    sum ratings for genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- a script that lists all genres in the database hbtn_0d_tvshows_rate
+-- by their rating.
+-- Each record should display: tv_genres.name - rating sum
+
+SELECT name, SUM(rate) AS 'rating sum'
+FROM tv_genres, tv_show_ratings, tv_show_genres WHERE tv_genres.id = genre_id
+AND tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name ORDER BY 'rating sum' DESC;
