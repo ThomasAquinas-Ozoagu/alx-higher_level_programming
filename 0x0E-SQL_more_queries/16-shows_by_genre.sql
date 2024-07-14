29bacc36ddefd7f27923ba3ad6000af88c1f4b20
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-14 17:41:26 +0000

    capitalize all keywords

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- all genres of the show Dexter
 
  SELECT DISTINCT title,
- CASE WHEN genre_id IS NULL THEN NULL ELSE name END as name
+ CASE WHEN genre_id IS NULL THEN NULL ELSE name END AS name
  FROM tv_genres, tv_shows LEFT JOIN tv_show_genres ON id = show_id
  WHERE tv_genres.id = genre_id OR genre_id IS NULL ORDER BY title, name;
