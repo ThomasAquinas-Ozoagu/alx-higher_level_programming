0a8f11cf35771884cc9a9eea3c4a0741bf1be8ba
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-07-14 15:08:07 +0000

    combine two tables that do not have any common key

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- a script that uses the hbtn_0d_tvshows database to lists
 -- all genres of the show Dexter
 
-SELECT name FROM tv_genres, tv_show_genres, tv_shows
-WHERE tv_shows.title = 'Dexter'
+SELECT title FROM tv_shows, tv_show_genres, tv_genres
+WHERE name = 'Comedy'
 AND tv_genres.id = tv_show_genres.genre_id
-AND show_id = tv_shows.id ORDER BY name;
+AND show_id = tv_shows.id ORDER BY title;
